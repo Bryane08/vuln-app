7c333461908635555cdff1c08101527a4cf74d88
Bryan E <bryane08@gmail.com>
2024-11-15 23:37:33 -0500

    Testing CI/CD after role fix

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,7 +3,7 @@ function Home() {
     return (
       <div className="p-8">
         <h1 className="text-2xl font-bold">Home Page</h1>
-        <p>Welcome to the Vulnerability Tracker App Home Page check!</p>
+        <p>Welcome to the Vulnerability Tracker App Home Page check 1, 2!</p>
       </div>
     );
   }
